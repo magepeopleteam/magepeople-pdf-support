df22560d3256cc3ba0d8f973917462c155036d1f
aamahin <aamahin@users.noreply.github.com>
2023-03-29 12:07:03 +0600

    new version

--- mage-pdf.php
+++ mage-pdf.php
@@ -3,7 +3,7 @@
 * Plugin Name: MagePeople PDF Support
 * Plugin URI: http://mage-people.com
 * Description: This is the main PDF Lib for PDF Ticket, You Need to keep active this plugin for working MagePeople Plugin which generate PDF Tickets.
-* Version: 2.0
+* Version: 3.0
 * Author: MagePeople Team
 * Author URI: http://www.mage-people.com/
 * Text Domain: magepeople-pdf-support
@@ -13,7 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) { die; } // Cannot access pages directly.
 if( ! class_exists( 'mPDF' ) ) {
     require 'inc/plugin-updates/plugin-update-checker.php';
     $ExampleUpdateChecker = PucFactory::buildUpdateChecker(
-        'https://vaincode.com/update/mpdf/mpdf.json',
+        'https://mage-people.com/update/mpdf.json',
         __FILE__
     );
     require_once( dirname(__FILE__) . "/lib/vendor/autoload.php");
