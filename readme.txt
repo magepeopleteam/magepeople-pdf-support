ac6514b624b5a04c452ca1ac33dffb25a8b303f0
mahin <a.a.mahin@gmail.com>
2025-03-14 12:34:38 +0600

    composer jason update

---
+++ readme.txt
@@ -0,0 +1,15 @@
+=== MagePeople PDF Support ===
+Contributors: magepeopleteam, aamahin
+Tags: pdf,mpdf
+Requires at least: 5.3
+Stable tag: trunk
+Tested up to: 6.7.1
+Requires PHP: 8.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This is the main PDF Lib for PDF Ticket, You Need to keep active this plugin for working MagePeople Plugin which generate PDF Tickets.
+
+== Description ==
+
+This is the main PDF Lib for PDF Ticket, You Need to keep active this plugin for working MagePeople Plugin which generate PDF Tickets.
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '4.0.0.x-dev',
+        'version' => '4.0.0.9999999-dev',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '1c3485817e3f162ffdcaf7b84815a04295c122ae',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '4.0.0.x-dev',
+            'version' => '4.0.0.9999999-dev',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '1c3485817e3f162ffdcaf7b84815a04295c122ae',
+            'dev_requirement' => false,
+        ),
+        'psr/log' => array(
+            'pretty_version' => '3.0.2',
+            'version' => '3.0.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/log',
+            'aliases' => array(),
+            'reference' => 'f16e1d5863e37f8d8c2a01719f5b34baa2b714d3',
+            'dev_requirement' => false,
+        ),
+    ),
+);
