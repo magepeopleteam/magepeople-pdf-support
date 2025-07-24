c6b3f769cc848b44faaf15be561ffde196b4daa0
mahin <a.a.mahin@gmail.com>
2025-07-24 08:54:36 +0600

    error fix for 8.4

--- lib/vendor/psr/log/src/NullLogger.php
+++ lib/vendor/psr/log/src/NullLogger.php
@@ -19,7 +19,7 @@ class NullLogger extends AbstractLogger
      *
      * @throws \Psr\Log\InvalidArgumentException
      */
-    public function log($level, string|\Stringable $message, array $context = []): void
+    public function log($level, $message, array $context = []): void
     {
         // noop
     }
